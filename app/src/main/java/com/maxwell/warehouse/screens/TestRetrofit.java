3fad68c6bf30fbe9cee92102945dcf3ab5532ece
Maximiliano Burgos <maximiliano@enyetech.com>
2015-09-15 14:55:55 -0300

    listas mas decentes

--- app/src/main/java/com/maxwell/warehouse/screens/TestRetrofit.java
+++ app/src/main/java/com/maxwell/warehouse/screens/TestRetrofit.java
@@ -1,13 +1,16 @@
 package com.maxwell.warehouse.screens;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 
 import com.maxwell.warehouse.R;
 import com.maxwell.warehouse.interfaces.LevaduraPodService;
 import com.maxwell.warehouse.models.Podcast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.Call;
@@ -19,13 +22,16 @@ import retrofit.Retrofit;
 /**
  * Created by Maximiliano on 15/09/15.
  */
-public class TestRetrofit extends Activity {
+public class TestRetrofit extends ListActivity {
     String API = "http://api.levadura.argentinageek.net";
+    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_podcast);
+
+        list = new ArrayList<>();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(API)
@@ -41,6 +47,15 @@ public class TestRetrofit extends Activity {
             public void onResponse(Response<List<Podcast>> response) {
                 // Get result Repo from response.body()
                 Log.i("debug_log", "nada");
+
+                for(int i = 0; i < response.body().size(); i++){
+                    list.add(response.body().get(i).getDescripcion());
+                }
+
+                ArrayAdapter<String> myAdapter = new ArrayAdapter <>(TestRetrofit.this,
+                        R.layout.item_main, R.id.nameItem, list);
+
+                setListAdapter(myAdapter);
             }
 
             @Override
