4efc90cecf1b1832fbe35112a30b60d48a67dff9
Maxwell <maxib2007@gmail.com>
2015-10-08 18:15:22 -0300

    scroll bar demo

--- app/src/main/java/com/maxwell/warehouse/fragments/ListUIFragment.java
+++ app/src/main/java/com/maxwell/warehouse/fragments/ListUIFragment.java
@@ -22,6 +22,7 @@ import com.maxwell.warehouse.screens.NewsFragment;
 import com.maxwell.warehouse.screens.ParallaxDemo;
 import com.maxwell.warehouse.screens.ParallaxWithCardView;
 import com.maxwell.warehouse.screens.RecycleWithFactory;
+import com.maxwell.warehouse.screens.ScrollBarDemo;
 import com.maxwell.warehouse.screens.SnackbarDemo;
 import com.maxwell.warehouse.screens.TestRetrofit;
 import com.maxwell.warehouse.screens.UITesting;
@@ -55,6 +56,7 @@ public class ListUIFragment extends Fragment {
         itemsList.add(new Items(RecycleWithFactory.class, "Recycle With Factory"));
         itemsList.add(new Items(DialogDemo.class, "Dialog With Parameters"));
         itemsList.add(new Items(FontManagerDemo.class, "Font Manager Demo"));
+        itemsList.add(new Items(ScrollBarDemo.class, "ScrollBar Demo"));
 
         RVAdapter adapter = new RVAdapter(itemsList, getActivity());
         mRecyclerView.setAdapter(adapter);
---
+++ app/src/main/java/com/maxwell/warehouse/screens/ScrollBarDemo.java
@@ -0,0 +1,61 @@
+package com.maxwell.warehouse.screens;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.maxwell.warehouse.R;
+import com.maxwell.warehouse.adapters.GenericAdapterFactory;
+import com.maxwell.warehouse.models.Beneficio;
+import com.maxwell.warehouse.models.Information;
+import com.maxwell.warehouse.models.PromoFeatured;
+import com.maxwell.warehouse.utils.FontManger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Maxwell on 08/10/2015.
+ */
+public class ScrollBarDemo extends AppCompatActivity {
+    private GenericAdapterFactory adapterFactory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scrollbar_layout);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.customRecyclerViewScroll);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        mRecyclerView.setHasFixedSize(false);
+
+        List<Information> mListInformation = new ArrayList<>();
+
+        for(int i=0; i< 10; i++){
+            if(i % 2 == 0){
+                mListInformation.add(new Beneficio("Titulo", "Texto", true));
+            }else{
+                mListInformation.add(new Beneficio("Titulo", "Texto", false));
+            }
+        }
+
+        GenericAdapterFactory featuredAdapter = new GenericAdapterFactory(mListInformation);
+        mRecyclerView.setAdapter(featuredAdapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.customToolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar!=null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
