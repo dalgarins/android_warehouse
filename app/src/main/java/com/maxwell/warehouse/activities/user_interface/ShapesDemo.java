e3546f1e0698c9b1cfe8f1869c033414543c9ab3
Maxwell <maxib2007@gmail.com>
2015-12-11 21:10:27 -0300

    cleaning drawables and adding others

---
+++ app/src/main/java/com/maxwell/warehouse/activities/user_interface/ShapesDemo.java
@@ -0,0 +1,15 @@
+package com.maxwell.warehouse.activities.user_interface;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Maxwell on 11/12/2015.
+ */
+public class ShapesDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
