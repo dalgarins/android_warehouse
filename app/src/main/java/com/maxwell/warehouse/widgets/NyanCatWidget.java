01968ca21ffaeb80436df3a21b30f573989786cb
Maxwell <maxib2007@gmail.com>
2015-12-13 14:14:48 -0300

    nyan cat widget

---
+++ app/src/main/java/com/maxwell/warehouse/widgets/NyanCatWidget.java
@@ -0,0 +1,22 @@
+package com.maxwell.warehouse.widgets;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.RemoteViews;
+
+import com.maxwell.warehouse.R;
+
+/**
+ * Created by Maxwell on 13/12/2015.
+ */
+public class NyanCatWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
